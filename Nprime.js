8f5119e8e0aa3d2f239a7bb760782514a326b2ce
NooR Nabiji <noornabiji4@gmail.com>
2018-08-14 01:22:03 +0530

    n prime number

---
+++ Nprime.js
@@ -0,0 +1,13 @@
+var number=100;
+		for (var counter = 0; counter <= number; counter++) {
+
+    var notPrime = false;
+    for (var i = 2; i <= counter; i++) {
+        if (counter%i===0 && i!==counter) {
+            notPrime = true;
+        }
+    }
+    if (notPrime === false) {
+                console.log(counter);
+    }
+}
